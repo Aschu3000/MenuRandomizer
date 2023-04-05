a71ce8fca44830394ac7bacdcbaf8cbeec16a2a6
Aschu-3000 <schu0239@algonquinlive.com>
2023-04-05 16:19:58 -0400

    Add project files.

---
+++ MenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuRandomizer
+{
+    internal class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Roast Beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+
+        public string[] Condiments = { "Yellow Mustard", "Brown Mustard", "Honey Mustard", "Mayo", "Relish", "French Dressing" };
+
+        public string[] Breads = { "Rye", "White", "Wheat", "Pumpernickel", "A Roll" };
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * 0.01M);
+            Price = price.ToString("c");
+        }
+    }
+
+}
